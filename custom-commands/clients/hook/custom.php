9af98d8f081728767f2be0ba6724b223d7bf05a0
Vladislav_TS <vladislav.tsyrdea@gmail.com>
2023-01-07 16:05:58 +0300

    hooks

---
+++ custom-commands/clients/hook/custom.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Хуки Клиентов
+ */
+
+
+/**
+ * Обновление полей формы
+ */
+$formFieldsUpdate = [];
+
+
+/**
+ * Определение пола по Отчеству
+ */
+if ( $requestData->patronymic ) {
+
+    if ( mb_substr( $requestData->patronymic, -2 ) === "ич" )
+        $formFieldsUpdate[ "gender" ] = "M";
+    elseif ( mb_substr( $requestData->patronymic, -2 ) === "на" )
+        $formFieldsUpdate[ "gender" ] = "W";
+
+} // if. $requestData->patronymic
+
+
+$API->returnResponse( $formFieldsUpdate );
