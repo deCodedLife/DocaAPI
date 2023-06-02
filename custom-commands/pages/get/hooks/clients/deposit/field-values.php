b212637466329ab3e2ee5c23b592dba6141a15fd
Vladislav_TS <vladislav.tsyrdea@gmail.com>
2023-06-02 12:43:51 +0300

    2 june 2023

--- custom-commands/pages/get/hooks/clients/deposit/field-values.php
+++ custom-commands/pages/get/hooks/clients/deposit/field-values.php
@@ -1,7 +1,1 @@
 <?php
-
-/**
- * Подстановка ID клиента
- */
-if ( ( $requestData->context->form == "deposit" ) && $requestData->context->row_id )
-    $formFieldValues[ "id" ] = $requestData->context->row_id;
