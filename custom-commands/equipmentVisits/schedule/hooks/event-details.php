e6725d006ad71627365f84c334b9308f49cbc03a
deCodedLife <dcodedlife@gmail.com>
2023-11-24 19:52:26 +0300

    Edit equipment schedule filter keys

--- custom-commands/equipmentVisits/schedule/hooks/event-details.php
+++ custom-commands/equipmentVisits/schedule/hooks/event-details.php
@@ -1,7 +1,5 @@
 <?php
 
-$API->returnResponse( "What?" );
-
 
 /**
  * Определение цвета Записи
--- custom-commands/pages/get/hooks/equipmentSchedule/index.php
+++ custom-commands/pages/get/hooks/equipmentSchedule/index.php
@@ -7,9 +7,9 @@ $userDetails = $API->DB->from( "users_stores" )
     ->fetch();
 
 $pageScheme[ "structure" ][ 0 ][ "settings" ][ "filters" ] = [
-    "performers_article" => "user_id",
-    "performers_table" => "users",
-    "performers_title" => "first_name",
+    "performers_article" => "equipment_id",
+    "performers_table" => "wearableEquipment",
+    "performers_title" => "title",
     "store_id" => $userDetails[ "store_id" ] ?? $API->DB->from( "stores" )
             ->limit(1)->fetch()[ "id" ],
     "start_at" =>  date( 'Y-m-d' )
