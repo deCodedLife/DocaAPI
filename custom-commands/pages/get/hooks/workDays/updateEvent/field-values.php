e839665686bb561d596656123d516e633386bc47
CodedLife <dcodedlife@gmail.com>
2023-12-22 07:40:22 +0300

    [ bugfix ] Forgot compare rule

--- custom-commands/pages/get/hooks/workDays/updateEvent/field-values.php
+++ custom-commands/pages/get/hooks/workDays/updateEvent/field-values.php
@@ -6,7 +6,7 @@ $ruleDetails = $API->DB->from( "workDays" )
     ->where( "id", $pageDetail[ "row_id" ] )
     ->fetch();
 
-if ( $ruleDetails[ "is_weekend" ] ) {
+if ( $ruleDetails[ "is_weekend" ] === 'Y' ) {
 
     $formFieldValues[ "event_from" ][ "is_visible" ] = false;
     $formFieldValues[ "event_to" ][ "is_visible" ] = false;
