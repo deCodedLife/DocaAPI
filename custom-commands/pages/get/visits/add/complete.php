619fcb5b3842fe4b668f1f238ac84419885247ab
deCodedLife <dcodedlife@gmail.com>
2024-04-01 18:34:17 +0300

    [ bugfix ] Fix accordion fields

--- custom-commands/pages/get/visits/add/complete.php
+++ custom-commands/pages/get/visits/add/complete.php
@@ -9,3 +9,5 @@ if ( $userDetails[ "role_id" ] == 8 ) {
     $response[ "data" ][ 1 ][ "settings" ][ "areas" ][ 0 ][ "blocks" ][ 1 ][ "fields" ][ 5 ][ "search" ] = "services";
 
 }
+
+$response[ "data" ][ 1 ][ "settings" ][ "areas" ][ 1 ][ "blocks" ][ 0 ][ "fields" ][ 1 ][ "is_required" ] = true;
