82c817752d0502eba52a0a665f6cf18aef724375
deCodedLife <dcodedlife@gmail.com>
2024-04-02 14:34:11 +0300

    [ bugfix ] Change logs on equipment visits

--- custom-commands/equipmentVisits/update/postfix.php
+++ custom-commands/equipmentVisits/update/postfix.php
@@ -30,6 +30,42 @@ if ( $requestData->id && $requestData->reason_id ) $API->DB->update( "equipmentV
     ->execute();
 
 
+if ( $requestData->status === "waited" ) {
+
+    $visitDetails = $API->DB->from( "equipmentVisits" )
+        ->where( "id", $requestData->id )
+        ->fetch();
+
+    $clientDetails = $API->DB->from( "clients" )
+        ->innerJoin( "equipmentVisits on equipmentVisits.client_id = clients.id" )
+        ->where( "equipmentVisits.id", $requestData->id )
+        ->fetch();
+
+    $clientName = $clientDetails[ "last_name" ];
+    if ( $clientDetails[ "first_name" ] ) $clientName .= " " . mb_substr( $clientDetails[ "first_name" ], 0, 1 ) . ".";
+    if ( $clientDetails[ "patronymic" ] ) $clientName .= " " . mb_substr( $clientDetails[ "patronymic" ], 0, 1 ) . ".";
+
+    /**
+     * Уведомление о добавлении Задачи
+     */
+    $API->addNotification(
+        "system_alerts",
+        "Приём пациента",
+        "Пациент $clientName ожидает приёма",
+        "info",
+        $visitDetails[ "user_id" ],
+        ""
+    );
+
+    /**
+     * Отправка события о добавлении Задачи
+     */
+    $API->addEvent( "notifications" );
+
+
+}
+
+
 /**
  * Отправка события об обновлении расписания
  */
