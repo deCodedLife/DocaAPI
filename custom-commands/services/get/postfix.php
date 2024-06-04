34bd96055825c45c27ed6c63ae04f0fb44a8d331
deCodedLife <dcodedlife@gmail.com>
2024-06-04 15:46:45 +0300

    [ extension ] Add public title for services

--- custom-commands/services/get/postfix.php
+++ custom-commands/services/get/postfix.php
@@ -1,10 +1,13 @@
 <?php
 
+// TODO: Снести к чёрту
 foreach ( $response[ "data" ] as $key => $service ) {
 
-    if ( $service[ "num" ] == "99999999ZZZZZZZ" ) $response[ "data" ][ $key ][ "num" ] = "";
+    if ( $service[ "num" ] == "99999999ZZZZZZZ" )
+        $response[ "data" ][ $key ][ "num" ] = "";
 
 }
+
 
 if ( $requestData->context->block == "form_list" || $requestData->context->block == "select" ) {
 
@@ -15,7 +18,8 @@ if ( $requestData->context->block == "form_list" || $requestData->context->block
         $workingTime = $API->DB->from( "workingTime" )
             ->where( "user", $API->request->data->users_id );
 
-        foreach ( $workingTime as $row ) $services_price[ intval( $row[ "row_id" ] ) ] = $row;
+        foreach ( $workingTime as $row )
+            $services_price[ intval( $row[ "row_id" ] ) ] = $row;
 
     }
 
@@ -26,10 +30,21 @@ if ( $requestData->context->block == "form_list" || $requestData->context->block
         if ( !$price ) $price = $service[ "price" ];
         $response[ "title" ] = "{$service[ "article" ]} {$service[ "title" ]} + {$price}";
 
-        if ( $API->isPublicAccount() && !empty( $service[ "public_title" ] ) ) $service[ "title" ] = $service[ "public_title" ];
+
         $response[ "data" ][ $key ] = $service;
 
     }
 
 }
 
+
+if ( $API->isPublicAccount() ) {
+
+    foreach ( $response[ "data" ] as $key => $service ) {
+
+        if ( !empty( $service[ "public_title" ] ) ) $service[ "title" ] = $service[ "public_title" ];
+        $response[ "data" ][ $key ] = $service;
+
+    }
+
+}
