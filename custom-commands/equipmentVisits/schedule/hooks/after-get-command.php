1623b3c35c2bf87ce6d05ef90ea039e0075fd8ae
deCodedLife <dcodedlife@gmail.com>
2024-06-25 12:45:02 +0300

    [ extension/bugfix ] (fix day planning period)

--- custom-commands/equipmentVisits/schedule/hooks/after-get-command.php
+++ custom-commands/equipmentVisits/schedule/hooks/after-get-command.php
@@ -3,7 +3,7 @@
 if ( !empty( $API->request->data->end_at ) ) $requestData->end_at = $API->request->data->end_at;
 if ( !empty( $API->request->data->start_at ) ) $requestData->start_at = $API->request->data->start_at;
 
-$requestData->end_at = date( 'Y-m-d H:i:s', strtotime( "+ 31 days" ) );
+$requestData->end_at =  date( "Y-m-d H:i:s", strtotime( "$requestData->start_at + 31 day" ) );
 //$API->request->data->end_at = date( 'Y-m-d H:i:s', strtotime( "+ 1 month" ) );
 
 $API->request->data->user_id = [ 0 ];
